dc33ae0a54000465cba6c4d1f6b1c2423ccc33e3
tastytea <tastytea@tastytea.de>
2019-01-21 00:01:29 +0100

    Fixed manpage script

--- build_manpage.sh
+++ build_manpage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ -n "${1}" ]]; then
+if [ -n "${1}" ]; then
     dir="$(dirname ${0})"
     cp -vf ${dir}/whyblocked.1.adoc .
     sed -Ei "s/(Revision: +)[0-9]+\.[0-9]\.[0-9]/\1${1}/" whyblocked.1.adoc
